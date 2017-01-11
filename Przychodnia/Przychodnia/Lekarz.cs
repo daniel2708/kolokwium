1e54e0587fd5e871c78c1a8231b4f912cfe4e774
daniel2708 <elvisss1995@gmail.com>
2017-01-11 14:59:38 +0100

    Add files via upload

---
+++ Przychodnia/Przychodnia/Lekarz.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Przychodnia
+{
+    class Lekarz : Osoba
+    {
+        private string specjalnosc;
+
+
+        public Lekarz(string imieNazwisko,string specjalnosc) :base(imieNazwisko)
+        {
+            this.specjalnosc = specjalnosc;
+        }
+    }
+}
